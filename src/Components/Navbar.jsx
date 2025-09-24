11b74e114a26030803bed1d315858748b894589e
dullah9150 <dullah9150@gmail.com>
2025-09-24 05:19:15 -0800

    tesr

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,23 +5,35 @@ import "./Navbar.css";
 export default function Navbar() {
   const [open, setOpen] = useState(false);
 
+  const toggleMenu = () => setOpen(!open);
+  const closeMenu = () => setOpen(false);
+
   return (
     <nav className="navbar">
       <div className="container nav-inner">
-        <Link to="/" className="logo">A2Z Bazaar</Link>
+        <Link to="/" className="logo" onClick={closeMenu}>
+          A2Z Bazaar
+        </Link>
 
         <button
           className="menu-toggle"
-          onClick={() => setOpen(!open)}
+          onClick={toggleMenu}
           aria-label="Toggle menu"
+          aria-expanded={open}
         >
-          {open ? "✖" : "☰"} {/* Show X when open, hamburger when closed */}
+          {open ? "✖" : "☰"} 
         </button>
 
         <ul className={`nav-links ${open ? "open" : ""}`}>
-          <li><Link to="/" onClick={() => setOpen(false)}>Home</Link></li>
-          <li><Link to="/about" onClick={() => setOpen(false)}>About</Link></li>
-          <li><Link to="/location" onClick={() => setOpen(false)}>Location</Link></li>
+          <li>
+            <Link to="/" onClick={closeMenu}>Home</Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={closeMenu}>About</Link>
+          </li>
+          <li>
+            <Link to="/location" onClick={closeMenu}>Location</Link>
+          </li>
         </ul>
       </div>
     </nav>
